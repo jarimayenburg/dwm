1e967b6b70b3b5e161cc4fd0e99c9ab7266ae844
Jari Maijenburg <jarimayenburg@hotmail.com>
2023-11-14 19:34:31 +0100

    Changed statusbar colors

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char col_white[]       = "#ffffff";
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
 	[SchemeNorm] =	 { col_gray3, col_gray1,  col_gray2 },
-	[SchemeSel]  =	 { col_gray4, col_cyan,   col_cyan },
+	[SchemeSel]  =	 { col_white, col_gray2,  col_cyan },
 	[SchemeWarn] =	 { col_black, col_yellow, col_red },
 	[SchemeUrgent]=	 { col_white, col_red,    col_red },
 };
