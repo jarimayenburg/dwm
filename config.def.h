dc6df62b29df1ba98cfcbdfc80fe459b16ac30b3
Jari Maijenburg <jarimayenburg@hotmail.com>
2023-11-16 10:44:38 +0100

    Updated config.def.h

--- config.def.h
+++ config.def.h
@@ -18,11 +18,11 @@ static const char col_yellow[]      = "#ffff00";
 static const char col_white[]       = "#ffffff";
 
 static const char *colors[][3]      = {
-	/*					fg         bg          border   */
-	[SchemeNorm] =	 { col_gray3, col_gray1,  col_gray2 },
-	[SchemeSel]  =	 { col_white, col_gray2,  col_cyan },
-	[SchemeWarn] =	 { col_black, col_yellow, col_red },
-	[SchemeUrgent]=	 { col_white, col_red,    col_red },
+	/*					 fg            bg          border   */
+	[SchemeNorm]   =	 { col_gray3,  col_gray1,  col_gray2 },
+	[SchemeSel]    =	 { col_white,  col_gray2,  col_cyan },
+	[SchemeWarn]   =	 { col_yellow, col_gray1,  col_red },
+	[SchemeUrgent] =	 { col_red,    col_gray1,  col_red },
 };
 
 /* tagging */
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
